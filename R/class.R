45bced600bae895f41e941a81e042b6ab5bcb9d2
Diego Diez <diego10ruiz@gmail.com>
2013-10-11 18:12:04 +0900

    - added functions to check and plot sequence hits match to the canonical motif (best_f) in motifData slot. For now only identical match is considered.

--- R/class.R
+++ R/class.R
@@ -23,8 +23,44 @@ setGeneric("nmotif")
 nseq = function(object) object@info$nseq
 setGeneric("nseq")
 
+motifNames=function(object) featureNames(object@motifs)
+setGeneric("motifNames")
+
+sequenceNames=function(object) sequenceNames(object@sequences)
+setGeneric("sequenceNames")
 
 
+getMotifMatchMatrix = function(object, motif) {
+  r=object@ranges
+  r_motif=r[r$motif_name==motif,]
+  
+  best_f=as.character(object@motifs[motif,]$best_f)
+  bs=strsplit(best_f,"")[[1]]
+  
+  ms=do.call(rbind,strsplit(as.character(r_motif$sequence_hit), ""))
+  colnames(ms)=bs
+  rownames(ms)=space(r_motif)
+  
+  m=matrix(0,ncol=nchar(best_f),nrow=nrow(r_motif))
+  colnames(m)=bs
+  rownames(m)=space(r_motif)
+  
+  for(k in 1:length(bs)) {
+    m[ms[,k]==bs[k],k]=1
+  }
+  m
+}
+setGeneric("getMotifMatchMatrix")
+
+plotMotifMatchMatrix = function(object, motif, ...) {
+  op=par(mar=c(2,1,2,1))
+  m=getMotifMatchMatrix(object, motif)
+  image(x=1:ncol(m),1:nrow(m),t(m[order(rowSums(m)),]),col=c("white","blue4"),main=paste("motif",motif),axes=FALSE,xlab="",ylab="")
+  mtext(colnames(m),at=1:ncol(m),side=1, ...)
+  box()
+  par(op)
+}
+setGeneric("plotMotifMatchMatrix")
 
 ##### DEPRECATED.
 setClass("MotifSet",
