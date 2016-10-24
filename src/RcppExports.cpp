cf359428d674de36e2437a414101389a3309d918
Diego Diez <diego10ruiz@gmail.com>
2016-10-25 01:27:57 +0900

    Working on reimplementation of align() in Rcpp.

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,14 +5,39 @@
 
 using namespace Rcpp;
 
-// timesTwo
-NumericVector timesTwo(NumericVector x);
-RcppExport SEXP motifTools_timesTwo(SEXP xSEXP) {
+// whichChar
+int whichChar(CharacterVector x, char q);
+RcppExport SEXP motifTools_whichChar(SEXP xSEXP, SEXP qSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< NumericVector >::type x(xSEXP);
-    rcpp_result_gen = Rcpp::wrap(timesTwo(x));
+    Rcpp::traits::input_parameter< CharacterVector >::type x(xSEXP);
+    Rcpp::traits::input_parameter< char >::type q(qSEXP);
+    rcpp_result_gen = Rcpp::wrap(whichChar(x, q));
+    return rcpp_result_gen;
+END_RCPP
+}
+// alignc
+List alignc(StringVector x, StringVector y, IntegerMatrix score_matrix, int gap_score);
+RcppExport SEXP motifTools_alignc(SEXP xSEXP, SEXP ySEXP, SEXP score_matrixSEXP, SEXP gap_scoreSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< StringVector >::type x(xSEXP);
+    Rcpp::traits::input_parameter< StringVector >::type y(ySEXP);
+    Rcpp::traits::input_parameter< IntegerMatrix >::type score_matrix(score_matrixSEXP);
+    Rcpp::traits::input_parameter< int >::type gap_score(gap_scoreSEXP);
+    rcpp_result_gen = Rcpp::wrap(alignc(x, y, score_matrix, gap_score));
+    return rcpp_result_gen;
+END_RCPP
+}
+// foo
+int foo();
+RcppExport SEXP motifTools_foo() {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    rcpp_result_gen = Rcpp::wrap(foo());
     return rcpp_result_gen;
 END_RCPP
 }
--- src/align.cpp
+++ src/align.cpp
@@ -1,31 +1,143 @@
 #include <Rcpp.h>
 using namespace Rcpp;
+using namespace std;
 
-// This is a simple example of exporting a C++ function to R. You can
-// source this function into an R session using the Rcpp::sourceCpp 
-// function (or via the Source button on the editor toolbar). Learn
-// more about Rcpp at:
-//
-//   http://www.rcpp.org/
-//   http://adv-r.had.co.nz/Rcpp.html
-//   http://gallery.rcpp.org/
-//
-//' timesTwo
-//' 
-//' timesTwo
-//' 
+// [[Rcpp::export]]
+int whichChar(CharacterVector x, char q) {
+  int idx = 0;
+  for (int k = 0; k < x.length(); k++) {
+    if (x[k] == q) {
+      idx = k;
+    }
+  }
+  return(idx);
+}
+
 //' @export
 // [[Rcpp::export]]
-NumericVector timesTwo(NumericVector x) {
-  return x * 2;
+List alignc(StringVector x, StringVector y, IntegerMatrix score_matrix, int gap_score = -1) {
+
+  // initialize variables.
+  CharacterVector srow = as<CharacterVector>(rownames(score_matrix));
+  CharacterVector scol = as<CharacterVector>(colnames(score_matrix));
+  IntegerMatrix m(x.length() + 1, y.length() + 1);
+  int nrow = m.nrow();
+  int ncol = m.ncol();
+  int rsize = nrow * ncol;
+  IntegerVector ri(rsize);
+  IntegerVector rj(rsize);
+  IntegerVector rii(rsize);
+  IntegerVector rjj(rsize);
+  CharacterVector rtype(rsize);
+  
+  // initialize the borders.
+  for (int i = 0; i < nrow; i++) {
+    m(i, 0) = 0;
+    int idx = 0 * nrow + i;
+    ri[idx] = i;
+    rj[idx] = 0;
+    rii[idx] = i - 1;
+    rjj[idx] = 0;
+    rtype[idx] = "v";
+  }
+  for (int j = 0; j < ncol; j++) {
+    m(0, j) = 0;
+    int idx = j * nrow + 0;
+    ri[idx] = 0;
+    rj[idx] = j;
+    rii[idx] = 0;
+    rjj[idx] = j - 1;
+    rtype[idx] = "h";
+  }
+  
+  // iterate and fill.
+  for (int i = 1; i < m.nrow(); i++) {
+    for (int j = 1; j < m.ncol(); j++) {
+      // diagonal.
+      int xi = whichChar(srow, x[j - 1]);
+      int yi = whichChar(srow, y[i - 1]);
+      int s = score_matrix(xi, yi);
+      int d = m(i - 1, j - 1) + s; // has to fix assigning real score above.
+      // horizonal
+      int h = m(i, j - 1) + gap_score;
+      // vertical
+      int v = m(i - 1, j) + gap_score;
+      
+      // check max.
+      int idx = j * nrow + i;
+      cout << idx << endl;
+      if (d > h && d > v) {
+        ri[idx] = i;
+        rj[idx] = j;
+        rii[idx] = i - 1;
+        rjj[idx] = j - 1;
+        rtype[idx] = "d";
+        m(i, j) = d;
+      } else if (h > d && h > v) {
+        ri[idx] = i;
+        rj[idx] = j;
+        rii[idx] = i;
+        rjj[idx] = j - 1;
+        rtype[idx] = "h";
+        m(i, j) = h;
+      } else if (v > d && v > h) {
+        ri[idx] = i;
+        rj[idx] = j;
+        rii[idx] = i - 1;
+        rjj[idx] = j;
+        rtype[idx] = "v";
+        m(i, j) = v;
+      }
+    }
+  }
+  
+  
+  // find indexes for max score.
+  int mv = 0;
+  int i = 0;
+  int j = 0;
+  for (int ii = 0; ii < nrow; ii++) {
+    for (int jj = 0; jj < ncol; jj++) {
+      if (m(ii, jj) > mv) {
+        //int k = j * nrow + i;
+        mv = m(ii, jj);
+        i = ii;
+        j = jj;
+      }
+    }
+  }
+  cout << "max_i: " << i << endl;
+  cout << "max_j: " << j << endl;
+  cout << "max_v: " << mv << endl;
+  
+  // backtrace.
+  int s = 0;
+  while (m(i, j) != 0) {
+    int idx = j * nrow + i;
+
+    if (rtype[idx] == "d") {
+      i = i - 1;
+      j = j - 1;
+      s += m(i, j);
+    }
+
+    if (rtype[idx] == "h") {
+      j = j - 1;
+    }
+
+    if (rtype[idx] == "v") {
+      i = i - 1;
+    }
+  }
+
+  //StringMatrix mm;
+  List ret; ret["total_score"] = s; ret["scores"] = m;
+  return(ret);
 }
 
 
-// You can include R code blocks in C++ files processed with sourceCpp
-// (useful for testing and development). The R code will be automatically 
-// run after the compilation.
-//
-
 /*** R
-timesTwo(42)
+library(Biostrings)
+data("BLOSUM62")
+alignc(c("A", "J"), c("H", "K"), score_matrix = BLOSUM62)
 */
