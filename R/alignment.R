1fdd13cf1a7085b2eca7b7962c0cc82f361eeb97
Diego Diez <diego10ruiz@gmail.com>
2016-10-24 17:50:41 +0900

    Reformat code.

--- R/alignment.R
+++ R/alignment.R
@@ -10,10 +10,10 @@
 #' 
 #' @examples
 #' align("AHSSFOO", "GOODFDKD", type = "global")$alignment
-align <- function(x1, x2, score.matrix, gap.score=-1, type="local", debug = TRUE) {
-  if(length(x1) == 1 && length(x2) == 1) {
-    x1 = strsplit(x1, "")[[1]]
-    x2 = strsplit(x2, "")[[1]]
+align <- function(x1, x2, score.matrix, gap.score = -1, type = "local", debug = TRUE) {
+  if (length(x1) == 1 && length(x2) == 1) {
+    x1 <- strsplit(x1, "")[[1]]
+    x2 <- strsplit(x2, "")[[1]]
   }
   
   type <- match.arg(type, c("global", "local"))
@@ -36,184 +36,184 @@ align <- function(x1, x2, score.matrix, gap.score=-1, type="local", debug = TRUE
   m
 }
 
-.sw <- function(x1, x2, score.matrix, gap.score=-1,debug=FALSE) {
-  l1 = length(x1)
-  l2 = length(x2)
+.sw <- function(x1, x2, score.matrix, gap.score = -1,debug = FALSE) {
+  l1 <- length(x1)
+  l2 <- length(x2)
   
   if(missing(score.matrix))
-    score.matrix <- .generateScoreMatrix(c(x1,x2))
+    score.matrix <- .generateScoreMatrix(c(x1, x2))
   
   # initialize.
-  m <- matrix(NA, ncol=l1+1, nrow=l2+1)
+  m <- matrix(NA, ncol = l1 + 1, nrow = l2 + 1)
   colnames(m) = c("", x1)
   rownames(m) = c("", x2)
-  m[1,]=0
-  m[,1]=0
+  m[1, ] <- 0
+  m[, 1] <- 0
   
   # fill
-  res = list()
-  res = c(res, lapply(2:ncol(m), function(j) data.frame(row=1, col=j, ii=1, jj=j-1, type="h")))
-  res = c(res, lapply(2:nrow(m), function(i) data.frame(row=i, col=1, ii=i-1, jj=1, type="v")))
-  for(i in 2:nrow(m)) {
-    for(j in 2:ncol(m)) {
+  res <- list()
+  res <- c(res, lapply(2:ncol(m), function(j) data.frame(row = 1, col = j, ii = 1, jj = j - 1, type = "h")))
+  res <- c(res, lapply(2:nrow(m), function(i) data.frame(row = i, col = 1, ii = i - 1, jj = 1, type = "v")))
+  for (i in 2:nrow(m)) {
+    for (j in 2:ncol(m)) {
       # diagonal.
-      d <- m[i-1,j-1] + score.matrix[x1[j-1],x2[i-1]]
+      d <- m[i - 1, j - 1] + score.matrix[x1[j - 1], x2[i - 1]]
       # horizonal
-      h <- m[i,j-1] + gap.score
+      h <- m[i, j - 1] + gap.score
       # vertical
-      v <- m[i-1,j] + gap.score
+      v <- m[i - 1, j] + gap.score
       # assign.
-      tmp = .checkMax(c(diag=d, hori=h, vert=v))
-      if(tmp<0) tmp[]<-0 # no negative scores.
-      if(tmp>0) {
+      tmp <- .checkMax(c(diag = d, hori = h, vert = v))
+      if (tmp < 0) tmp[] <- 0 # no negative scores.
+      if (tmp > 0) {
         switch(names(tmp),
                diag = {
-                 res = c(res, list(data.frame(row=i, col=j, ii=i-1, jj=j-1, type="d")))
+                 res <- c(res, list(data.frame(row = i, col = j, ii = i - 1, jj = j - 1, type = "d")))
                },
                hori = {
-                 res = c(res, list(data.frame(row=i, col=j, ii=i, jj=j-1, type="h")))
+                 res <- c(res, list(data.frame(row = i, col = j, ii = i, jj = j - 1, type = "h")))
                },
                vert = {
-                 res = c(res, list(data.frame(row=i, col=j, ii=i-1, jj=j, type="v")))
+                 res <- c(res, list(data.frame(row = i, col = j, ii = i - 1, jj = j, type = "v")))
                }
         )
       }
-      m[i,j] <- tmp
+      m[i, j] <- tmp
     }
   }
-  res = do.call(rbind, res)
+  res <- do.call(rbind, res)
   
   # backtrace
-  index = which(m == max(m), arr.ind = TRUE)
-  index<-index[nrow(index),] # there may be several max-- get the last one!
+  index <- which(m == max(m), arr.ind = TRUE)
+  index <- index[nrow(index), ] # there may be several max-- get the last one!
   i <- index[1]
   j <- index[2]
-  r1 = c()
-  r2 = c()
+  r1 <- c()
+  r2 <- c()
   st <- 0
   repeat {
-    if(m[i,j]==0) break
+    if(m[i, j] == 0) break
     
-    tmp <- subset(res, row==i & col==j)
+    tmp <- subset(res, row == i & col == j)
     switch(as.character(tmp$type),
            d = {
-             r1 = c(r1, x1[j-1])
-             r2 = c(r2, x2[i-1])
-             st = st + m[i,j]
-             i=i-1
-             j=j-1
+             r1 <- c(r1, x1[j - 1])
+             r2 <- c(r2, x2[i - 1])
+             st <- st + m[i, j]
+             i <- i - 1
+             j <- j - 1
            },
            v = {
-             r1 = c(r1, "-")
-             r2 = c(r2, x2[i-1])
-             i=i-1
+             r1 <- c(r1, "-")
+             r2 <- c(r2, x2[i - 1])
+             i <- i - 1
            },
            h = {
-             r1 = c(r1, x1[j-1])
-             r2 = c(r2, "-")
-             j=j-1
+             r1 <- c(r1, x1[j - 1])
+             r2 <- c(r2, "-")
+             j <- j - 1
            }
     )
   }
   
   # build alignment.
-  if(length(r1)>0) {
-    al=data.frame(rbind(rev(r1),rev(r2)),row.names = c("s1","s2"),check.names = FALSE)
-    colnames(al) = 1:ncol(al)
+  if (length(r1) > 0) {
+    al <- data.frame(rbind(rev(r1), rev(r2)), row.names = c("s1", "s2"), check.names = FALSE)
+    colnames(al) <- 1:ncol(al)
   }
   else
-    al=data.frame()
+    al <- data.frame()
   
   # results.
-  if(debug)
-    list(score.matrix=score.matrix, gap.score=gap.score, scores=m, traceback=res, sequences=list(s1=x1,s2=x2), score=st, alignment=al)
+  if (debug)
+    list(score.matrix = score.matrix, gap.score = gap.score, scores = m, traceback = res, sequences = list(s1 = x1, s2 = x2), score = st, alignment = al)
   else
-    list(alignment=al, score=st)
+    list(alignment = al, score = st)
 }
 
 
-.nw <- function(x1, x2, score.matrix, gap.score=-1, debug=FALSE) {
-  l1 = length(x1)
-  l2 = length(x2)
+.nw <- function(x1, x2, score.matrix, gap.score = -1, debug = FALSE) {
+  l1 <- length(x1)
+  l2 <- length(x2)
   
-  if(missing(score.matrix))
-    score.matrix <- .generateScoreMatrix(c(x1,x2))
+  if (missing(score.matrix))
+    score.matrix <- .generateScoreMatrix(c(x1, x2))
   
   # initialize.
-  m <- matrix(NA, ncol=l1+1, nrow=l2+1)
-  colnames(m) = c("", x1)
-  rownames(m) = c("", x2)
-  m[1,]=-1*(0:l1)
-  m[,1]=-1*(0:l2)
+  m <- matrix(NA, ncol = l1 + 1, nrow = l2 + 1)
+  colnames(m) <- c("", x1)
+  rownames(m) <- c("", x2)
+  m[1, ] = -1 * (0:l1)
+  m[, 1] = -1 * (0:l2)
   
   # fill
-  res = list()
-  res = c(res, lapply(2:ncol(m), function(j) data.frame(row=1, col=j, ii=1, jj=j-1, type="h")))
-  res = c(res, lapply(2:nrow(m), function(i) data.frame(row=i, col=1, ii=i-1, jj=1, type="v")))
-  for(i in 2:nrow(m)) {
-    for(j in 2:ncol(m)) {
+  res <- list()
+  res <- c(res, lapply(2:ncol(m), function(j) data.frame(row = 1, col = j, ii = 1, jj = j - 1, type = "h")))
+  res <- c(res, lapply(2:nrow(m), function(i) data.frame(row = i, col = 1, ii = i - 1, jj = 1, type = "v")))
+  for (i in 2:nrow(m)) {
+    for (j in 2:ncol(m)) {
       # diagonal.
-      d <- m[i-1,j-1] + score.matrix[x1[j-1],x2[i-1]]
+      d <- m[i - 1, j - 1] + score.matrix[x1[j - 1], x2[i - 1]]
       # horizonal
-      h <- m[i,j-1] + gap.score
+      h <- m[i, j - 1] + gap.score
       # vertical
-      v <- m[i-1,j] + gap.score
+      v <- m[i - 1, j] + gap.score
       # assign.
-      tmp = .checkMax(c(diag=d, hori=h, vert=v))
+      tmp = .checkMax(c(diag = d, hori = h, vert = v))
       switch(names(tmp),
              diag = {
-               res = c(res, list(data.frame(row=i, col=j, ii=i-1, jj=j-1, type="d")))
+               res <- c(res, list(data.frame(row = i, col = j, ii = i - 1, jj = j - 1, type = "d")))
              },
              hori = {
-               res = c(res, list(data.frame(row=i, col=j, ii=i, jj=j-1, type="h")))
+               res <- c(res, list(data.frame(row = i, col = j, ii = i, jj = j - 1, type = "h")))
              },
              vert = {
-               res = c(res, list(data.frame(row=i, col=j, ii=i-1, jj=j, type="v")))
+               res <- c(res, list(data.frame(row = i, col = j, ii = i - 1, jj = j, type = "v")))
              }
       )
-      m[i,j] <- tmp
+      m[i, j] <- tmp
     }
   }
-  res = do.call(rbind, res)
+  res <- do.call(rbind, res)
   
   # backtrace
   i <- nrow(m)
   j <- ncol(m)
-  r1 = c()
-  r2 = c()
+  r1 <- c()
+  r2 <- c()
   st <- 0
   repeat {
-    tmp <- subset(res, row==i & col==j)
+    tmp <- subset(res, row == i & col == j)
     switch(as.character(tmp$type),
            d = {
-             r1 = c(r1, x1[j-1])
-             r2 = c(r2, x2[i-1])
-             st = st + m[i,j]
-             i=i-1
-             j=j-1
+             r1 <- c(r1, x1[j-1])
+             r2 <- c(r2, x2[i-1])
+             st <- st + m[i, j]
+             i <- i - 1
+             j <- j - 1
            },
            v = {
-             r1 = c(r1, "-")
-             r2 = c(r2, x2[i-1])
-             i=i-1
+             r1 <- c(r1, "-")
+             r2 <- c(r2, x2[i - 1])
+             i <- i - 1
            },
            h = {
-             r1 = c(r1, x1[j-1])
-             r2 = c(r2, "-")
-             j=j-1
+             r1 <- c(r1, x1[j - 1])
+             r2 <- c(r2, "-")
+             j <- j - 1
            }
     )
-    if(i==1 && j==1) break
+    if(i == 1 && j == 1) break
   }
   
   # build alignment.
-  al=data.frame(rbind(rev(r1),rev(r2)),row.names = c("s1","s2"),check.names = FALSE)
+  al <- data.frame(rbind(rev(r1), rev(r2)), row.names = c("s1", "s2"), check.names = FALSE)
   
   # results.
-  if(debug)
-    list(score.matrix=score.matrix, gap.score=gap.score, scores=m, traceback=res, sequences=list(s1=x1,s2=x2), score=st, alignment=al)
+  if (debug)
+    list(score.matrix = score.matrix, gap.score = gap.score, scores = m, traceback = res, sequences = list(s1 = x1, s2 = x2), score = st, alignment = al)
   else
-    list(alignment=al, score=st)
+    list(alignment = al, score = st)
 }
 
 #' Align two motifs represented as PWMs
@@ -255,6 +255,6 @@ motifAlign <- function(x1, x2, align.method = "local", score.method = "PCC", deb
   align(colnames(m1), colnames(m2), score.matrix = s, debug = debug, type = align.method)
 }
 
-motifDistance <- function(x1, x2, score.method="PCC", align.method="local", debug=TRUE) {
-  motifAlign(x1,x2,score.method=score.method, align.method=align.method, debug=debug)$score
+motifDistance <- function(x1, x2, score.method = "PCC", align.method = "local", debug = TRUE) {
+  motifAlign(x1, x2, score.method = score.method, align.method = align.method, debug = debug)$score
 }
