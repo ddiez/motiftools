1c35098907e0b65e967b19c52acc38e161e190cf
Diego Diez <diego10ruiz@gmail.com>
2016-10-27 23:40:13 +0900

    Rewrite of readMEMEnew() with xml2. For now ignore motif models.

--- R/test.R
+++ R/test.R
@@ -1,119 +1,131 @@
-readMEME2 <- function(file, description = NULL) {
+# handy function.
+getAttr <- function(id, attr, info) {
+  info[info["id"] == id, attr]
+}
+getAttr <- Vectorize(getAttr, vectorize.args = "id", USE.NAMES = FALSE)
+
+# get alphabet.
+getAlphabet <- function(x) {
+  p <- xml_find_first(x, ".//training_set/alphabet")
+  d <- lapply(1:xml_length(p), function(i) {
+    tmp <- xml_attrs(xml_child(p, i))
+    data.frame(id = tmp["id"], symbol = tmp["symbol"], name = tmp["name"], stringsAsFactors = FALSE, row.names = tmp["id"])
+  })
+  do.call(rbind, d)
+}
+
+# get motifs.
+getMotifs <- function(x) {
+  p <- xml_find_first(x, ".//motifs")
+  d <- lapply(1:xml_length(p), function(i) {
+    tmp <- xml_attrs(xml_child(p, i))
+    data.frame(id = tmp["id"], name = tmp["name"], width = as.numeric(tmp["width"]), stringsAsFactors = FALSE, row.names = tmp["name"])
+  })
+  do.call(rbind, d)  
+}
+
+# get sequences.
+getSequences <- function(x) {
+  p <- xml_find_all(x, ".//sequence")
+  tmp <- do.call(rbind, xml_attrs(p))
+  data.frame(
+    id = tmp[, "id"],
+    name = tmp[, "name"],
+    length = as.numeric(tmp[, "length"]),
+    weight = as.numeric(tmp[, "weight"]),
+    stringsAsFactors = FALSE,
+    row.names = tmp[, "name"]
+  )
+}
+
+# further sequence info (nsites and p.value).
+getSeqStats <- function(x) {
+  p <- xml_find_all(x, ".//scanned_sites")
+  
+  tmp <- do.call(rbind, xml_attrs(p))
+  data.frame(
+    sequence_id = tmp[, "sequence_id"],
+    p.value = as.numeric(tmp[, "pvalue"]),
+    nsites = as.integer(tmp[, "num_sites"]),
+    stringsAsFactors = FALSE,
+    row.names = tmp[, "sequence_id"]
+  )
+}
+
+# get motif hits.
+getMotifHits <- function(x, motif_info) {
+  p <- xml_find_all(root, ".//scanned_sites")
+  
+  hits <- lapply(p, function(node) {
+    seq_info <- xml_attr(node, "sequence_id")
+    tmp <- do.call(rbind, xml_attrs(xml_children(node)))
+    data.frame(
+      sequence_id = seq_info,
+      motif_id = tmp[, "motif_id"],
+      position = as.integer(tmp[, "position"]) + 1,
+      strand = tmp[, "strand"],
+      p.value = as.numeric(tmp[, "pvalue"]),
+      stringsAsFactors = FALSE
+    )
+  })
+  hits <- do.call(rbind, hits)
+  getMotifWidth <- function(id, motif_info) {
+    motif_info[motif_info$id == id, "width"]
+  }
+  hits$width <- getAttr(motif_hit$motif_id, attr = "width", info = motif_info)
+  #hits$width <- motif_info[hits$motif_id, "width"]
+  hits
+}
+
+#' readMEMEnew
+#' 
+#' readMEMEnew
+#'
+#' @param file XML output file from MEME.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+readMEMEnew <- function(file) {
   library(xml2)
-  #f <- "~/projects/motiftools/inst/files/meme_ras/meme.xml"
   doc <- read_xml(file)
   root <- xml_root(doc)
   
-  # alphabet
-  p <- xml_find_first(root, ".//training_set/alphabet")
-  d <- lapply(1:xml_length(p), function(i) {
-    tmp <- xml_attrs(xml_child(p, i))
-    data.frame(id = tmp["id"], symbol = tmp["symbol"], name = tmp["name"], stringsAsFactors = FALSE, row.names = NULL)
-  })
-  alphabet <- do.call(rbind, d)
-  #alphabet
+  alf_info <- getAlphabet(root)
+  seq_info <- getSequences(root)
+  motif_info <- getMotifs(root)
+  motif_hit <- getMotifHits(root, motif_info)
   
-  # motifs.
-  p <- xml_find_first(root, ".//motifs")
-  d <- lapply(1:xml_length(p), function(i) {
-    tmp <- xml_attrs(xml_child(p, i))
-    data.frame(id = tmp["id"], name = tmp["name"], width = as.numeric(tmp["width"]), stringsAsFactors = FALSE, row.names = tmp["id"])
-  })
-  motif.info <- do.call(rbind, d)
-  #motif.info
-  
-  # probabilities.
-  p <- xml_find_all(root, ".//probabilities")
-  length(p) # one per motif.
-  motif.prob <- lapply(p, function(n) {
-    tmp <- sapply(xml_find_all(n, ".//alphabet_array"), function(l) {
-      l <- xml_children(l)
-      txt <- xml_text(l)
-      # print(txt)
-      names(txt) <- xml_attr(l, "letter_id")
-      txt
-    })
-    mode(tmp) <- "numeric"
-    colnames(tmp) <- 1:ncol(tmp)
-    tmp
-  })
-  names(motif.prob) <- motif.info$id
-  #motif.prob
-  
-  # scores.
-  p <- xml_find_all(root, ".//scores")
-  length(p) # one per motif.
-  motif.score <- lapply(p, function(n) {
-    tmp <- sapply(xml_find_all(n, ".//alphabet_array"), function(l) {
-      l <- xml_children(l)
-      txt <- xml_text(l)
-      # print(txt)
-      names(txt) <- xml_attr(l, "letter_id")
-      txt
-    })
-    mode(tmp) <- "numeric"
-    colnames(tmp) <- 1:ncol(tmp)
-    tmp
-  })
-  #motif.score
-  
-  # scanned_sites_summary.
-  p <- xml_find_all(root, ".//scanned_sites")
-  
-  tmp <- do.call(rbind, xml_attrs(p))
-  seq.info <-
-    data.frame(
-      sequence_id = tmp[, "sequence_id"],
-      p.value = as.numeric(tmp[, "pvalue"]),
-      nsites = as.integer(tmp[, "num_sites"]),
-      stringsAsFactors = FALSE
-    )
-  rownames(seq.info) <- seq.info$sequence_id
-  #seq.info
-  
-  
-  motif.site <- lapply(p, function(n) {
-    tmp <- sapply(xml_find_all(p[[1]], ".//scanned_site"), function(s) {
-      xml_attrs(s)
-    })
-    data.frame(
-      motif_id = tmp["motif_id",],
-      position = as.integer(tmp["position",]) + 1,
-      width = motif.info[tmp["motif_id",], "width"],
-      p.value = as.numeric(tmp["pvalue",]),
-      stringsAsFactors = FALSE)
-  })
-  names(motif.site) <- seq.info$sequence_id
-  motif.site <- do.call(rbind, motif.site)
-  #motif.site
-  
-  # range.
-  r <- RangedData(
-    IRanges(start = motif.site$position, width = motif.site$width),
-    motif_name = motif.site$motif_id,
-    p.value = motif.site$p.value,
-    space = motif.site$sequence_id
-  )
+  # alphabetData.
+  alphabetData <- AnnotatedDataFrame(alf_info)
   
   # sequenceData.
-  sequenceData <- AnnotatedDataFrame(seq.info)
+  sequenceData <- AnnotatedDataFrame(seq_info)
   
   # motifData.
-  motifData <- AnnotatedDataFrame(motif.info)
+  motifData <- AnnotatedDataFrame(motif_info)
   
-  nseq <- nrow(seq.info)
-  nmotif <- nrow(motif.info)
+  # rangeData.
+  rangeData <- RangedData(
+    IRanges(start = motif_hit$position, width = motif_hit$width),
+    motif_name = getAttr(motif_hit$motif_id, attr = "name", info = motif_info),
+    p.value = motif_hit$p.value,
+    space = getAttr(motif_hit$sequence_id, attr = "name", info = seq_info)
+  )
+  
   new(
     "MotifSearchResult",
     info = list(
       tool = "MEME",
       description = description,
-      nseq = nseq,
-      nmotif = nmotif
+      nseq = nrow(seq_info),
+      nmotif = nrow(motif_info),
+      alphabet = alphabetData
     ),
     sequences = sequenceData,
     motifs = motifData,
-    models = motif.prob,
-    ranges = r
+    models = list(),
+    ranges = rangeData
   )
 }
