f248b062dc4e69361e7798dddd4808c39c97edba
Diego Diez <diego10ruiz@gmail.com>
2016-10-27 17:00:43 +0900

    Restrict imports from ape package.

--- R/help.R
+++ R/help.R
@@ -3,7 +3,8 @@
 #' motifTools
 #' 
 #' @name motifTools
-#' @import methods Biobase IRanges Biostrings XML knitr ape
+#' @import methods Biobase IRanges Biostrings XML knitr
 #' @importFrom Rcpp evalCpp
+#' @importFrom ape as.phylo plot.phylo
 #' @useDynLib motifTools
 NULL
