e15bc0591076a3d82b2b4690d81edf86ed84be53
Diego Diez <diego10ruiz@gmail.com>
2016-10-27 16:54:44 +0900

    Fixed/modified unit test.

--- tests/testthat/test_align.R
+++ tests/testthat/test_align.R
@@ -1,5 +1,7 @@
 library(motifTools)
 
 test_that("alignment", {
-  expect_equal(align("FOO", "FOO")$alignment[1, 1], "F")
+  library(Biostrings)
+  data(BLOSUM62)
+  expect_equal(as.character(align("ALA", "ALA", score.matrix = BLOSUM62, type = "global")$alignment[1, 1]), "A")
 })
