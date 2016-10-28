4516c87674a99049e7c0b6c0b9320b7bb0d782b3
Diego Diez <diego10ruiz@gmail.com>
2016-10-28 23:36:46 +0900

    Fixed dependencies for vignette to compile. Need to look at those Imports in more detail (many warnings).

--- R/help.R
+++ R/help.R
@@ -3,7 +3,7 @@
 #' motiftools
 #' 
 #' @name motiftools
-#' @import methods Biobase IRanges Biostrings XML xml2 ape gtable
+#' @import methods Biobase IRanges Biostrings XML xml2 ape gtable ggplot2 reshape2 dplyr
 #' @importFrom Rcpp evalCpp
 #' @importFrom grid grid.newpage grid.draw
 #' @importFrom ggdendro ggdendrogram
