3bc220c5a50002b1c453302edcbfc600ed986b72
Diego Diez <diego10ruiz@gmail.com>
2016-10-29 18:15:58 +0900

    Methods are now exported.

--- tests/testthat/test_meme.R
+++ tests/testthat/test_meme.R
@@ -4,9 +4,9 @@ f <- system.file("files/meme_ras/meme.xml", package = "motiftools")
 x <- readMEME(f)
 
 test_that("slots", {
-  expect_identical(motiftools:::nmotif(x), 9L)
-  expect_identical(motiftools:::nseq(x), 4L)
-  expect_equal(motiftools:::sequenceNames(x), c("RASH_MOUSE", "RASK_MOUSE", "RASN_MOUSE", "RASM_HUMAN"))
-  expect_equal(motiftools:::motifNames(x), as.character(1:9))
+  expect_identical(nmotif(x), 9L)
+  expect_identical(nseq(x), 4L)
+  expect_equal(sequenceNames(x), c("RASH_MOUSE", "RASK_MOUSE", "RASN_MOUSE", "RASM_HUMAN"))
+  expect_equal(motifNames(x), as.character(1:9))
 })
 
