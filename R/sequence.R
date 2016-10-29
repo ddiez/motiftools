f83d5afe31dd5b943986dd63b24350ed283951be
Diego Diez <diego10ruiz@gmail.com>
2016-10-29 11:38:15 +0900

    Reworked plotConservationMatrix.

--- R/sequence.R
+++ R/sequence.R
@@ -48,27 +48,46 @@ plotConservation = function(x, seq.names = FALSE, cluster.row = TRUE) {
   invisible(g)
 }
 
-#' @title plotConservationMatrix
-#' @description plots a heatmap with the conservation score for each residue
+#' plotConservationMatrix
+#' 
+#' plots a heatmap with the conservation score for each residue
+#'
 #' @param x a matrix as obtained with conservationMatrix()
-#' @param tree a tree of class phylo (package ape)
+#' @param tree a tree of class dendrogram or that can be coerced into.
 plotConservationMatrix <- function(x, tree) {
-  if(!missing(tree))
-    x <- x[tree$tip, ]
+  if (!missing(tree)) {
+    tree <- as.dendrogram(tree)
+    x <- x[order.dendrogram(tree), ]
+  }
+
+  nr <- nrow(x)
   
   # tree grob.
-  ptree <- ggtree(tree, branch.length = "none", ladderize = FALSE)
-  gtree <- ggplotGrob(ptree + theme(plot.margin = unit(c(1,0,1,1), "lines")) + scale_y_discrete())
+  ptree <- suppressMessages(
+    ggdendrogram(tree, rotate = FALSE, theme_dendro = FALSE) +
+      coord_flip() +
+      scale_x_continuous(limits = c(.5, nr + .5), expand = c(0, 0)) +
+      scale_y_reverse() + 
+      theme_void()
+    )
+  gtree <- ggplotGrob(ptree)
   
   # heatmap.
   d <- melt(x, varnames = c("sequences","position"), value.name = "conservation")
   d$conservation <- factor(d$conservation, levels = 1:5, labels =  c("gap (-)","< 40%",">= 40%",">= 60%",">= 80%"))
   
-  gheat <- ggplot(d, aes(x = position, y = sequences, fill = conservation)) +
-    geom_tile() + ylab(NULL) + scale_x_discrete() + scale_y_discrete() + theme(panel.margin = unit(c(0,0,0,0), "null"), axis.ticks = element_blank(), axis.text = element_blank(), plot.margin = unit(c(1,1,1,0), "lines")) + scale_fill_manual(values=c("white","grey80","grey50", "steelblue","orange"), limits = c("gap (-)","< 40%",">= 40%",">= 60%",">= 80%"), guide="legend")
+  pheat <- ggplot(d, aes(x = position, y = sequences, fill = conservation)) + 
+    geom_tile() + 
+    scale_y_discrete(expand = c(0, 0)) +
+    scale_fill_manual(values = c("white", "grey80", "grey50", "steelblue", "orange"),
+                      limits = c("gap (-)", "< 40%", ">= 40%", ">= 60%", ">= 80%"), guide = "legend")
+  gheat <- ggplotGrob(pheat)
   
-  g <- gtable_add_cols(gtree, unit(5, "null"))
-  g <- gtable_add_grob(g, ggplotGrob(gheat), t = 1, b = nrow(g), l = ncol(g))
+  g <- gtable(widths = unit(c(1, 5, 1), "null"), heights = unit(c(5, 1), "null"))
+  g <- gtable_add_grob(g, gtable_filter(gtree, "panel"), t = 1, l = 1)
+  g <- gtable_add_grob(g, gtable_filter(gheat, "panel"), t = 1, l = 2)
+  g <- gtable_add_grob(g, gtable_filter(gheat, "guide-box"), t = 1, l =3)
+  g <- gtable_add_grob(g, gtable_filter(gheat, "xlab"), t = 2, l = 2)
   grid.newpage()
   grid.draw(g)
   invisible(g)
