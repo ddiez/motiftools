52bfb20ca14b4293cea2f523971781a843c74b5a
Diego Diez <diego10ruiz@gmail.com>
2016-10-29 18:18:17 +0900

    Added unit tests for subsetting.

--- tests/testthat/test_meme.R
+++ tests/testthat/test_meme.R
@@ -6,6 +6,8 @@ x <- readMEME(f)
 test_that("slots", {
   expect_identical(nmotif(x), 9L)
   expect_identical(nseq(x), 4L)
+  expect_identical(nmotif(x[, 1]), 1L)
+  expect_identical(nseq(x[1, ]), 1L)
   expect_equal(sequenceNames(x), c("RASH_MOUSE", "RASK_MOUSE", "RASN_MOUSE", "RASM_HUMAN"))
   expect_equal(motifNames(x), as.character(1:9))
 })
