86d5d952eb7555cd1be6b83fea2db86981883068
Diego Diez <diego10ruiz@gmail.com>
2016-10-30 23:07:44 +0900

    Reimplemented readFIMO with xml2 (with speed up gain). Old one renamed to readFIMO_old (to be removed).

--- R/fimo.R
+++ R/fimo.R
@@ -1,5 +1,148 @@
+#' Read results from FIMO motif search stored in XML format.
+#' 
+#' Read results from FIMO motif search stored in XML format and returns a
+#' MotifSearchResult object.
+#'
+#' @param file file XML output file from FIMO.
+#' @param description 
+#'
+#' @return MotifSearchResult object.
+#' @export
+#'
+#' @examples
+#' NULL
+readFIMO <- function(file, description = NULL) {
+  # parse fimo.xml.
+  doc <- read_xml(file)
+  root <- xml_root(doc)
+  
+  nseq <- getFimoSequenceInfo(root)$nseq
+  motif_info <- getFimoMotifInfo(root)
+  nmotif <- nrow(motif_info)
+  alpha_bkgd <- getFimoBackground(root)
+  cisml_file <- getFimoCisml(root)
+  cisml_file <- file.path(dirname(file), cisml_file)
+  
+  # parse cisml.xml.
+  doc <- read_xml(cisml_file)
+  root <- xml_root(doc)
+  
+  motif_hits <- getFimoMotifHits(root)
+  
+  seq_names <- unique(motif_hits$seq_id)
+  seq_info <- data.frame(seq_names, sequence_id = seq_names, row.names = 1, stringsAsFactors = FALSE)
+  
+  if (nrow(motif_hits) > 0) {
+    motif_hits <-
+      RangedData(
+        IRanges(start = motif_hits$start, width = motif_info[motif_hits$motif_name, "width"]),
+        motif_name = motif_hits$motif_name,
+        score = motif_hits$score,
+        pvalue = motif_hits$pvalue,
+        qvalue = motif_hits$qvalue,
+        evalue = rep(NA, nrow(motif_hits)),
+        sequence = motif_hits$sequence,
+        space = motif_hits$seq_id)
+  } else {
+    motif_hits <- RangedData()
+  }
+  
+  motifData <- AnnotatedDataFrame(motif_info)
+  sequenceData <- AnnotatedDataFrame(seq_info)
+  
+  new(
+    "MotifSearchResult",
+    info = list(
+      tool = "FIMO",
+      description = description,
+      nseq = nseq,
+      nmotif = nmotif
+    ),
+    sequences = sequenceData,
+    motifs = motifData,
+    ranges = motif_hits
+  )
+}
+
+# motif info.
+getFimoMotifInfo <- function(x) {
+  tmp <- do.call(rbind, xml_attrs(xml_find_all(x, "motif")))
+  data.frame(
+    name = tmp[, "name"],
+    width = as.integer(tmp[, "width"]),
+    best_match = tmp[, "best-possible-match"],
+    stringsAsFactors = FALSE
+  )
+}
+
+# sequence background.
+getFimoBackground <- function(x) {
+  tmp <- xml_find_all(x, ".//background/value")
+  data.frame(
+    letter = xml_attr(tmp, "letter"),
+    background = xml_double(tmp),
+    stringsAsFactors = FALSE
+  )
+}
+
+# cisml file.
+getFimoCisml <- function(x) {
+  xml_text(xml_find_first(x, "cisml-file")) 
+}
+
+# alphabet.
+getFimoAlphabet <- function(x) {
+  xml_text(xml_find_first(x, "alphabet"))
+}
+
+# sequence info.
+getFimoSequenceInfo <- function(x) {
+  tmp <- xml_attrs(xml_find_first(x, "sequence-data"))
+  data.frame(
+    nseq = as.integer(tmp["num-sequences"]),
+    nresidues = as.integer(tmp["num-residues"])
+  )
+}
+
+# motif hits.
+getFimoMotifHits <- function(x) {
+  tmp_motif <- lapply(xml_children(x), function(motif) { # iterate over motifs ("pattern").
+    # check we are in a pattern.
+    if (xml_name(motif) == "pattern") {
+      motif_name <- xml_attr(motif, "name")
+      tmp_seq <- lapply(xml_children(motif), function(seq) { # iterate over sequences.
+        # check we are in a sequence.
+        if (xml_name(seq) == "scanned-sequence") {
+          seq_id <- xml_attr(seq, "accession")
+          tmp_match <- lapply(xml_children(seq), function(hit) { # iterate over hits.
+            # check we are in a hit.
+            if (xml_name(hit) == "matched-element") {
+              seq_str <- xml_text(xml_contents(hit)[[1]])
+              qvalue <- xml_double(xml_contents(hit)[[2]])
+              data.frame(
+                motif_name = motif_name,
+                seq_id = seq_id,
+                start = as.integer(xml_attr(hit, "start")),
+                score = as.numeric(xml_attr(hit, "score")),
+                pvalue = as.numeric(xml_attr(hit, "pvalue")),
+                qvalue = qvalue,
+                sequence = seq_str,
+                stringsAsFactors = FALSE
+              )
+            }
+          })
+          bind_rows(tmp_match)
+        }
+      })
+      bind_rows(tmp_seq)
+    }
+  })
+  bind_rows(tmp_motif)
+}
+
+### Legacy code.
 ## reads XML FIMO output.
-readFIMO = function(filename, sequenceData, description=NULL) {
+readFIMO_old = function(filename, sequenceData, description=NULL) {
   doc = xmlParse(filename)
   top = xmlRoot(doc)
   
--- R/help.R
+++ R/help.R
@@ -5,7 +5,7 @@
 #' @name motiftools
 #' @import methods Biobase Biostrings XML xml2 gtable ggplot2
 #' @importFrom reshape2 melt
-#' @importFrom dplyr filter select mutate group_by summarize %>%
+#' @importFrom dplyr filter select mutate group_by summarize bind_rows %>%
 #' @importFrom IRanges IRanges RangedData space
 #' @importFrom Rcpp evalCpp
 #' @importFrom grid grid.newpage grid.draw
