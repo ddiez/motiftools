1384afbe4eb7fc9f599dcb1157f86bfefd5b8a77
Diego Diez <diego10ruiz@gmail.com>
2016-10-30 23:27:12 +0900

    Added FIMO example data and unit test.

---
+++ tests/testthat/test_fimo.R
@@ -0,0 +1,13 @@
+library(motiftools)
+
+f <- system.file("files/fimo_ras/fimo.xml", package = "motiftools")
+x <- readFIMO(f)
+
+test_that("slots", {
+  expect_identical(nmotif(x), 9L)
+  expect_identical(nseq(x), 4L)
+  expect_identical(nmotif(x[, 1]), 1L)
+  expect_identical(nseq(x[1, ]), 1L)
+  expect_equal(sequenceNames(x), c("RASH_MOUSE", "RASK_MOUSE", "RASN_MOUSE", "RASM_HUMAN"))
+  expect_equal(motifNames(x), as.character(1:9))
+})
