d4483e0b7fdac7c0dab26b8fa93e855b5dc635b0
Diego Diez <diego10ruiz@gmail.com>
2016-11-08 23:22:27 +0900

    Fixed documentation.

--- R/sequence.R
+++ R/sequence.R
@@ -37,13 +37,20 @@ conservationMatrix <- function(object) {
 #' @param color color for the tile borders (default: transparent).
 #' 
 #' @export
-plotConservationMatrix <- function(x, tree, color = "transparent") {
+plotConservationMatrix <- function(x, tree, color = "transparent", show.tips = FALSE, show.consensus = FALSE, size = 8) {
   if (!missing(tree)) {
     tree <- as.phylo(tree)
     x <- x[tree$tip.label, ]
   }
 
   nr <- nrow(x)
+  labels <- NULL
+  if (show.consensus)
+    labels <- colnames(x)
+  tips <- NULL
+  if (show.tips)
+    tips <- rownames(x)
+  colnames(x) <- seq_len(ncol(x))
   
   # tree grob.
   ptree <- ggtree(tree, branch.length = "none") + 
@@ -52,22 +59,46 @@ plotConservationMatrix <- function(x, tree, color = "transparent") {
   
   # heatmap.
   d <- melt(x, varnames = c("sequences","position"), value.name = "conservation")
+  d$sequences <- factor(d$sequences)
+  d$position <- factor(d$position)
   d$conservation <- factor(d$conservation, levels = 1:5, labels =  c("gap (-)","< 40%",">= 40%",">= 60%",">= 80%"))
-  
+
   pheat <- ggplot(d, aes_string(x = "position", y = "sequences", fill = "conservation")) + 
     geom_tile(color = color) + 
-    scale_x_discrete(expand = c(0, 0)) +
-    scale_y_discrete(expand = c(0, 0)) +
+    scale_x_discrete(expand = c(0, 0), labels = labels) +
+    scale_y_discrete(expand = c(0, 0), labels = tips) +
     scale_fill_manual(values = c("white", "grey80", "grey50", "steelblue", "orange"),
-                      limits = c("gap (-)", "< 40%", ">= 40%", ">= 60%", ">= 80%"), guide = "legend")
+                      limits = c("gap (-)", "< 40%", ">= 40%", ">= 60%", ">= 80%"), guide = "legend") + 
+    theme(axis.ticks = element_blank(), axis.text.x = element_text(size = size))
   gheat <- ggplotGrob(pheat)
   
-  g <- gtable(widths = unit(c(1, 5, 1.5), "null"), heights = unit(c(5, 1), c("null", "lines")))
-  g <- gtable_add_grob(g, gtable_filter(gtree, "panel"), t = 1, l = 1)
-  g <- gtable_add_grob(g, gtable_filter(gheat, "panel"), t = 1, l = 2)
-  g <- gtable_add_grob(g, gtable_filter(gheat, "guide-box"), t = 1, l =3)
-  g <- gtable_add_grob(g, gtable_filter(gheat, "xlab"), t = 2, l = 2)
+  gt <- gtable(widths = unit(c(.5, 5), "null"), heights = unit(5, "null"))
+  gt <- gtable_add_grob(gt, gtable_filter(gtree, "panel"), t = 1, l = 1)
+  gt <- gtable_add_grob(gt, gtable_filter(gheat, "panel"), t = 1, l = 2)
+  
+  if (show.tips) {
+    gg <- gtable_filter(gheat, "axis-l")
+    gt <- gtable_add_cols(gt, widths = gg$widths, pos = 1)
+    gt <- gtable_add_grob(gt, gg, t = 1, l = 2)
+  }
+  
+  if (show.consensus) {
+    gg <- gtable_filter(gheat, "axis-b")
+    gt <- gtable_add_rows(gt, heights = gg$heights, pos = 0)
+    gt <- gtable_add_row_space(gt, unit(0.5, "lines"))
+    gt <- gtable_add_grob(gt, gg, t = 1, l = -1)
+  }
+
+  gt <- gtable_add_rows(gt, heights = unit(1, "lines"), pos = -1)
+  gt <- gtable_add_grob(gt, gtable_filter(gheat, "xlab"), t = -1, l = -1)
+    
+  gg <- gtable_filter(gheat, "guide-box")
+  gt <- gtable_add_cols(gt, widths = gg$widths, pos = -1)
+  gt <- gtable_add_grob(gt, gg, t = -2, l = -1)
+  
+  gt <- gtable_add_padding(gt, unit(.5, "lines"))
   grid.newpage()
-  grid.draw(g)
-  invisible(g)
+  grid.draw(gt)
+  invisible(gt)
 }
+
