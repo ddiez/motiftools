13f0c39796358025a5bc50e5ed0a37f53b9aa45b
Diego Diez <diego10ruiz@gmail.com>
2016-11-10 22:10:10 +0900

    Added unit test for local alignment. Removed coercion to character (returned alignment now is matrix of character).

--- tests/testthat/test_align.R
+++ tests/testthat/test_align.R
@@ -1,5 +1,6 @@
 library(motiftools)
 
 test_that("alignment", {
-  expect_equal(as.character(align("ALA", "ALA", score.matrix = "BLOSUM62", type = "global")$alignment[1, 1]), "A")
+  expect_equal(align("ALA", "ALA", score.matrix = "BLOSUM62", type = "global")$alignment[1, 1], "A")
+  expect_equal(align("ALA", "ALA", score.matrix = "BLOSUM62", type = "local")$alignment[1, 1], "A")
 })
