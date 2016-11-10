44abc8df2db52f8c320e08de01c1c80470e01d21
Diego Diez <diego10ruiz@gmail.com>
2016-11-10 22:25:19 +0900

    Added unit test for getScoreMatrix.

--- tests/testthat/test_misc.R
+++ tests/testthat/test_misc.R
@@ -3,3 +3,11 @@ context("Testing miscellaneous functions")
 test_that("dcor", {
   expect_equal(dcor(t(matrix(rep(1:10, 2), ncol = 2)))[1], 0)
 })
+
+test_that("getScoreMatrix", {
+  m <- getScoreMatrix("BLOSUM62")
+  expect_true(nrow(m) == 25L)
+  expect_true(ncol(m) == 25L)
+  expect_true(m["H", "H"] == 8L)
+  expect_true(m["A", "W"] == -3L)
+})
