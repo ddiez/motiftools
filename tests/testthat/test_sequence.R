118e2d15c23d88459ed825979192121359d1b500
Diego Diez <diego10ruiz@gmail.com>
2016-11-10 14:21:07 +0900

    Improvements to conservationMatrix unit tests.

--- tests/testthat/test_sequence.R
+++ tests/testthat/test_sequence.R
@@ -3,8 +3,10 @@ library(Biostrings)
 
 f <- system.file("files/ras.faln", package = "motiftools")
 aln <- readAAMultipleAlignment(f)
+cons <- conservationMatrix(aln)
 
 test_that("conservation", {
-  expect_equal(conservationMatrix(aln)[1,1], 1L)
-  expect_equal(conservationMatrix(aln)[1,14], 5L)
+  expect_type(cons, "integer")
+  expect_true(cons[1,1] == 1L)
+  expect_true(cons[1,14] == 5L)
 })
