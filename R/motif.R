145adbb38d853b0da218a7408fa077297ae63da1
Diego Diez <diego10ruiz@gmail.com>
2016-11-14 16:29:20 +0900

    Reformat code.

--- R/motif.R
+++ R/motif.R
@@ -44,81 +44,75 @@ function(object, simplify = TRUE, filter = FALSE) {
 
 #### TO CHECK:
 
-getCoverage = function(object) {
-  r=reduce(object@ranges)
-  pdata=pData(object@sequences)
+getCoverage <- function(object) {
+  r <- reduce(object@ranges)
+  pdata <- pData(object@sequences)
   sapply(names(r), function(n) {
-    100*sum(width(r[n]))/pdata[n,"length"]
+    100 * sum(width(r[n]))/pdata[n, "length"]
   })
 }
 setGeneric("getCoverage")
 
-getTotalCoverage = function(object) {
-  r=reduce(object@ranges)
-  pdata=pData(object@sequences)
-  100*sum(width(r))/sum(pdata[,"length"])
+getTotalCoverage <- function(object) {
+  r <- reduce(object@ranges)
+  pdata <- pData(object@sequences)
+  100 * sum(width(r))/sum(pdata[, "length"])
 }
 setGeneric("getTotalCoverage")
 
-getMotifBySeq = function(object) {
-  lapply(object@ranges, function(x) x$motif_name) # for RangedData.
+getMotifBySeq <- function(object) {
+  lapply(object@ranges, function(x) x$motif_name)  # for RangedData.
 }
 setGeneric("getMotifBySeq")
 
-getMotifArchBySeq = function(object) {
-  sapply(getMotifBySeq(object),function(x) paste(x,collapse="-"))
+getMotifArchBySeq <- function(object) {
+  sapply(getMotifBySeq(object), function(x) paste(x, collapse = "-"))
 }
 setGeneric("getMotifArchBySeq")
 
 # convert architectures.
-.letter2num = function(x){
-  .architecture.code=c(LETTERS,letters,as.character(0:9))
-  x=strsplit(x,"")[[1]]
-  as.character(sapply(x,function(z) which(.architecture.code %in% z),USE.NAMES=FALSE))
+.letter2num <- function(x) {
+  .architecture.code <- c(LETTERS, letters, as.character(0:9))
+  x <- strsplit(x, "")[[1]]
+  as.character(sapply(x, function(z) which(.architecture.code %in% z), USE.NAMES = FALSE))
 }
 
-.num2letter = function(x){
-  .architecture.code=c(LETTERS,letters,as.character(0:9))
-  paste(.architecture.code[as.numeric(x)],collapse="")
+.num2letter <- function(x) {
+  .architecture.code <- c(LETTERS, letters, as.character(0:9))
+  paste(.architecture.code[as.numeric(x)], collapse = "")
 }
 
-convertArch = function(object,to="string") {
-  to=match.arg(to,c("number","string"))
-  switch(to,
-         "number"=sapply(object,.letter2num),
-         "string"=sapply(object,.num2letter)
-  )
+convertArch <- function(object, to = "string") {
+  to <- match.arg(to, c("number", "string"))
+  switch(to, number = sapply(object, .letter2num), string = sapply(object, .num2letter))
 }
 
-getMotifDistribution=function(object,by.groups) {
-  m=object
-  if(!missing(by.groups))
-    rownames(m)=by.groups
+getMotifDistribution <- function(object, by.groups) {
+  m <- object
+  if (!missing(by.groups)) 
+    rownames(m) <- by.groups
   
-  nu=unique(rownames(m))
-  tmp=lapply(nu,function(n) {
-    sel.n=rownames(m) %in% n
-    l=length(which(sel.n))
-    100*apply(m[sel.n,,drop=FALSE],2,sum)/l
+  nu <- unique(rownames(m))
+  tmp <- lapply(nu, function(n) {
+    sel.n <- rownames(m) %in% n
+    l <- length(which(sel.n))
+    100 * apply(m[sel.n, , drop = FALSE], 2, sum)/l
   })
-  names(tmp)=nu
-  do.call(rbind,tmp)
+  names(tmp) <- nu
+  do.call(rbind, tmp)
 }
 setGeneric("getMotifDistribution")
 
-setMethod("getMotifDistribution","MotifSearchResult",
-function(object,by.groups) {
-  m=getMotifMatrix(object)
-  getMotifDistribution(m,by.groups)
+setMethod("getMotifDistribution", "MotifSearchResult", function(object, by.groups) {
+  m <- getMotifMatrix(object)
+  getMotifDistribution(m, by.groups)
 })
 
 
-getMotifCount = function(object, percentage = FALSE) {
-  apply(getMotifMatrix(object),2,function(m) {
-    if(percentage)
-      100*sum(m)/nseq(object)
-    else
-      sum(m)
+getMotifCount <- function(object, percentage = FALSE) {
+  apply(getMotifMatrix(object), 2, function(m) {
+    if (percentage) 
+      100 * sum(m)/nseq(object) else sum(m)
   })
 }
 setGeneric("getMotifCount")
