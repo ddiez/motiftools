8f6ea23fb0b8ff16987a422cbffec9a788702e46
Diego Diez <diego10ruiz@gmail.com>
2016-11-20 11:50:49 +0900

    Added unit test for plotConservationMatrix.

--- tests/testthat/test_sequence.R
+++ tests/testthat/test_sequence.R
@@ -1,11 +1,27 @@
 context("Testing sequence functions")
 
 f <- system.file("files/ras.faln", package = "motiftools")
-aln <- readAAMultipleAlignment(f)
-cons <- conservationMatrix(aln)
 
-test_that("conservation", {
+test_that("conservationMatrix works", {
+  aln <- readAAMultipleAlignment(f)
+  cons <- conservationMatrix(aln)
+  
   expect_type(cons, "integer")
   expect_true(cons[1,1] == 1L)
   expect_true(cons[1,14] == 5L)
+})
+
+test_that("plotConservationMatrix works", {
+  aln <- readAAMultipleAlignment(f)
+  BiocGenerics::rownames(aln) <- sub(" .*", "", BiocGenerics::rownames(aln)) # need to fix sequence names.
+  
+  library(ape)
+  tree <- read.tree(system.file("files/ras.tree", package = "motiftools"))
+  
+  p <- plotConservationMatrix(aln, tree, show.tips = TRUE)
+  expect_is(p, "gtable")
+  
+  cons <- conservationMatrix(aln)
+  p <- plotConservationMatrix(cons, tree)
+  expect_is(p, "gtable")
 })
